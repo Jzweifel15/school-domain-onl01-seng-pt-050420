63a2bfecb2dc4cfb16044811346c7c61c93bfb4e
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-05-19 17:51:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,18 @@
 # code here!
 class School
   
+  attr_reader :name, :roster
+  
+  ROSTER = {}
+  
   def initialize(name, roster)
     @name = name
-    @roster = Hash.new
+    @roster = roster
+  end
+  
+  def roster=(roster)
+    @roster = roster
+    @roster = {}
   end
   
 end
